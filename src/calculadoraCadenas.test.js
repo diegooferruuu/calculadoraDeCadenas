0b3d24d5df1cf000074703447ba9a7448dd26cf1
diegooferruuu <diego.ferrufino@ucb.edu.bo>
2024-11-20 21:04:16 -0400

    Refactoring: Se mejoro la claridad de las pruebas jest

--- src/calculadoraCadenas.test.js
+++ src/calculadoraCadenas.test.js
@@ -1,32 +1,67 @@
 import calcularSumaDeCadena from "./calculadoraCadenas.js";
 
-describe("Calcular", () => {
-  it("deberia devolver 0", () => {
-    expect(calcularSumaDeCadena(' ')).toEqual(0);
+describe("Calcular suma de cadenas", () => {
+  describe("Casos básicos", () => {
+    it("debería devolver 0 para una cadena vacía o solo espacios", () => {
+      const inputVacio = " ";
+      const resultadoEsperado = 0;
+
+      expect(calcularSumaDeCadena(inputVacio)).toEqual(resultadoEsperado);
+    });
+
+    it("debería devolver el número si la cadena contiene solo un número", () => {
+      const inputUnNumero = "2";
+      const resultadoEsperado = 2;
+
+      expect(calcularSumaDeCadena(inputUnNumero)).toEqual(resultadoEsperado);
+    });
+
+    it("debería sumar números separados por comas", () => {
+      const inputComas = "1,2,4,8";
+      const resultadoEsperado = 15;
+
+      expect(calcularSumaDeCadena(inputComas)).toEqual(resultadoEsperado);
+    });
+
+    it("debería sumar números separados por comas o guiones", () => {
+      const inputComasGuiones = "1-2,3";
+      const resultadoEsperado = 6;
+
+      expect(calcularSumaDeCadena(inputComasGuiones)).toEqual(resultadoEsperado);
+    });
+
+    it("debería ignorar números mayores o iguales a 1001", () => {
+      const inputIgnorarGrandes = "2,1001";
+      const resultadoEsperado = 2;
+
+      expect(calcularSumaDeCadena(inputIgnorarGrandes)).toEqual(resultadoEsperado);
+    });
   });
-  it("deberia devolver 2", () => {
-    expect(calcularSumaDeCadena('2')).toEqual(2);
-  });
-  it("deberia devolver 15", () => {
-    expect(calcularSumaDeCadena('1,2,4,8')).toEqual(15);
-  });
-  it("deberia devolver 6", () => {
-    expect(calcularSumaDeCadena('1-2,3')).toEqual(6);
-  });
-  it("deberia devolver 17", () => {
-    expect(calcularSumaDeCadena('//[;] 6;7;4')).toEqual(17);
-  });
-  it("deberia devolver 12", () => {
-    expect(calcularSumaDeCadena("//[;] 6,3-2;1")).toEqual(12);
-  });
-  it("deberia devolver 2", () => {
-    expect(calcularSumaDeCadena("2, 1001")).toEqual(2);
-  });
-  it("deberia devolver 22", () => {
-    expect(calcularSumaDeCadena("//[*][%] 1*2%3,7-9")).toEqual(22);
-  });
-  it("deberia devolver 25", () => {
-    expect(calcularSumaDeCadena("//[*][%][+] 1*2%3,7-9+3,1001")).toEqual(25);
+
+  describe("Delimitadores personalizados", () => {
+    it("debería aceptar un delimitador simple entre corchetes", () => {
+      const inputUnDelimitador = "//[;] 6;7;4";
+      const resultadoEsperado = 17;
+
+      expect(calcularSumaDeCadena(inputUnDelimitador)).toEqual(resultadoEsperado);
+    });
+
+    it("debería aceptar múltiples delimitadores entre corchetes", () => {
+      const inputMultiplesDelimitadores = "//[*][%] 1*2%3,7-9";
+      const resultadoEsperado = 22;
+
+      const inputMultiplesDelimitadoresConIgnorados = "//[*][%][+] 1*2%3,7-9+3,1001";
+      const resultadoEsperadoConIgnorados = 25;
+
+      expect(calcularSumaDeCadena(inputMultiplesDelimitadores)).toEqual(resultadoEsperado);
+      expect(calcularSumaDeCadena(inputMultiplesDelimitadoresConIgnorados)).toEqual(resultadoEsperadoConIgnorados);
+    });
+
+    it("debería funcionar con delimitadores personalizados junto con comas o guiones", () => {
+      const inputMixto = "//[;] 6,3-2;1";
+      const resultadoEsperado = 12;
+
+      expect(calcularSumaDeCadena(inputMixto)).toEqual(resultadoEsperado);
+    });
   });
 });
-
